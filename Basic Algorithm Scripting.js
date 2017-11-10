8dcffa0f01397ac5a24c2503f8126e9b68b47c75
Michelle <michellebrettcr@gmail.com>
2017-11-10 20:08:59 +1100

    Completed FCC Basic Algorithm Challenge

---
+++ Basic Algorithm Scripting.js
@@ -0,0 +1,22 @@
+## Convert Celsius to Fahrenheit
+
+/*The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheitalready defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature. Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.*/
+
+function convertToF(celsius) {
+  return fahrenheit = (celsius * 9/5)+32;
+}
+
+convertToF(30);
+
+
+## Reverse a String
+
+/*Reverse the provided string.
+You may need to turn the string into an array before you can reverse it.*/
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+reverseString("hello");
