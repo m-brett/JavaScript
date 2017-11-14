bc1418f0594a5ca1fab5dab6322c6eb4f679da42
Michelle <michellebrettcr@gmail.com>
2017-11-15 10:51:44 +1100

    Complete Basic Algorithm Challenge

--- Basic Algorithm Scripting.js
+++ Basic Algorithm Scripting.js
@@ -165,11 +165,10 @@ findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }) should
 findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }) should return undefined.*/
 
 	function findElement(arr, func) {
-	  var num = 0;
-	  return num;
+	  return arr.filter(func)[0];
 	}
 
-	findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
+	findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
 
 
 
