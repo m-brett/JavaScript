963a795a5164f1fa544cc777bb96274392beff93
Michelle <michellebrettcr@gmail.com>
2017-11-16 10:09:28 +1100

    Completed Basic Algorithm Challege

--- Basic Algorithm Scripting.js
+++ Basic Algorithm Scripting.js
@@ -216,6 +216,49 @@ booWho("false") should return false. */
 
 
 
+## Title Case a Sentence
+
+/* Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+
+TESTS
+titleCase("I'm a little tea pot") should return a string.
+titleCase("I'm a little tea pot") should return I'm A Little Tea Pot.
+titleCase("sHoRt AnD sToUt")should return Short And Stout.
+titleCase("HERE IS MY HANDLE HERE IS MY SPOUT") should return Here Is My Handle Here Is My Spout.*/
+
+	function titleCase(str) {
+	  var lower = str.toLowerCase().split(" ");
+	  for (var i=0; i<lower.length; i++){
+		  lower[i] = lower[i].substr(0,1).toUpperCase() + lower[i].substring(1);
+	  }
+	 return lower.join(" ");
+	}
+
+	titleCase("I'm a little tea pot");
+
+
+
+
+## Falsy Bouncer
+
+/* Remove all falsy values from an array.
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+Hint: Try converting each value to a Boolean.
+
+TESTS
+bouncer([7, "ate", "", false, 9]) should return [7, "ate", 9].
+bouncer(["a", "b", "c"])should return ["a", "b", "c"].
+bouncer([false, null, 0, NaN, undefined, ""]) should return [].
+bouncer([1, null, NaN, 2, undefined]) should return [1, 2]. */
+
+	function bouncer(arr) {
+	  // Don't show a false ID to this bouncer.
+	  return arr;
+	}
+
+	bouncer([7, "ate", "", false, 9]);
+
 
 
 
