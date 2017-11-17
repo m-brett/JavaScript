5620e6af63dd34dc0a241ac4365a61306c58e1cb
Michelle <michellebrettcr@gmail.com>
2017-11-18 10:06:19 +1100

    Complete Basic Algorithm Challenge

--- Basic Algorithm Scripting.js
+++ Basic Algorithm Scripting.js
@@ -359,5 +359,20 @@ chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], 
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]]*/
 
 
+	function chunkArrayInGroups(arr, size) {
+  		var final = [];
+  		for (var i = 0; i< arr.length; i+size){
+    		final.push(arr.splice(i, size)); 
+  		}  
+  		return final;
+	}
+
+	chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+
+
+
+
+
 
 
