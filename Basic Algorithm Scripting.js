82dea87ef274f4968b65d637cdede039da474bbf
Michelle <michellebrettcr@gmail.com>
2017-11-18 10:05:59 +1100

    Completed Basic Algorithm Challenge

--- Basic Algorithm Scripting.js
+++ Basic Algorithm Scripting.js
@@ -310,7 +310,7 @@ getIndexToIns([2, 20, 10], 19);
 
 ## Mutations
 
-/*Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+/* Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
 For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
 The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
 Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
@@ -328,10 +328,20 @@ mutation(["voodoo", "no"])should return false.*/
 
 
 	function mutation(arr) {
-	  return arr;
+  		var first = arr[0].toLowerCase();
+  		var second = arr[1].toLowerCase().split("");
+  
+		  for (var i = 0; i<second.length; i++){
+			var place = first.indexOf(second[i]);
+			  if (place == -1){
+				return false;
+			  }
+		   }
+		  return true;
 	}
 
-	mutation(["hello", "hey"]);
+	mutation(["Mary", "Army"]);
+
 
 
 
@@ -347,15 +357,6 @@ chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]]*/
-
-
-	function chunkArrayInGroups(arr, size) {
-	  // Break it up.
-	  return arr;
-	}
-
-	chunkArrayInGroups(["a", "b", "c", "d"], 2);
-
 
 
 
