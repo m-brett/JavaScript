fbbc8512ed25d40a6ccba9f52c1e1aa5a66ef507
Michelle <michellebrettcr@gmail.com>
2017-11-19 11:19:41 +1100

    Complete Intermediate Algotihm Challege

--- Intermediate Algorithm Scripting (FCC).js
+++ Intermediate Algorithm Scripting (FCC).js
@@ -96,16 +96,18 @@ palindrome("five|\_/|four")should return false. */
 
 
 	function palindrome(str) {
-		// Good luck!
-		return true;
+  
+  		var stripped = str.replace(/[\W_]+/g,"").toLowerCase();
+  		var reversed = stripped.split("").reverse().join("");
+  
+  		return stripped == reversed ? true : false;
 	}
 
-
-	palindrome("eye");
-
+	palindrome("0_0 (: /-\ :) 0-0");
 
 
-## Intermediate Algorithm Scripting: Seek and Destroy
+
+## Seek and Destroy
 
 /*You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
 You have to use the arguments object.
@@ -117,6 +119,7 @@ destroyer([3, 5, 1, 2, 2], 2, 3, 5) should return [1].
 destroyer([2, 3, 2, 3], 2, 3)should return [].
 destroyer(["tree", "hamburger", 53], "tree", 53)should return ["hamburger"].
 destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan")should return [12,92,65].*/
+
 
 	function destroyer(arr) {
 	  // Remove all the values
---
+++ Project Euler Scripting.js
@@ -0,0 +1,16 @@
+# Solutions created for Project Euler Problems
+
+
+## Problem 1: Multiples of 3 and 5
+
+/* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below the provided parameter value number. */
+
+
+	function euler1(number) {
+	  // Good luck!
+	  return true;
+	}
+
+	euler1(1000);
